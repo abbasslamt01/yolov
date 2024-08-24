4c8a941994120faa09ca8b579731a4e35d12d882
abbas-slamt <abbasslmt01@gmail.com>
2024-08-24 02:02:47 -0400

    add cam

--- yolov.py
+++ yolov.py
@@ -1,3 +1,16 @@
 import cv2
-import streamlit
+import streamlit as st
 
+camera = cv2.VideoCapture(0)
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+fre = st.image([])
+
+
+
+while run:
+    _, frame = camera.read()
+
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    fre.image(frame)
